3bd2752f29227575ccc499ee0007ffe613aa267e
zeena kareem <zena34269@gmail.com>
2020-05-28 10:55:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById('time').value;
+ displayMessage(greet(timeString))
+ console.log(greet(timeString))
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+let e
+function greet(timearg){
+   e=timearg.split(':');
+  timearg= e.parseInt()
+  if(timearg<12){
+    return "Good Morning";
+  }
+  else if(timearg>=12 & timearg<=5){
+    return  "Good Afternoon";
+    
+  }
+  else{
+    return "Good Evening";
+  }
+
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(time2){
+ document. getElementById('greeting').innerHTML=`${time2}`;
+}
+
+
+
+
+
